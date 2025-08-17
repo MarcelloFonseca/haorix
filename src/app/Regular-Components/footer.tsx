0dc183dcf1207148fa0f3754cb57bd7a224af52f
Marcello Fonseca <marcellofonseca@MacBook-Pro-2.local>
2025-08-17 01:03:07 -0400

    Ajout du logo nav+footer

--- src/app/Regular-Components/footer.tsx
+++ src/app/Regular-Components/footer.tsx
@@ -1,17 +1,52 @@
-import { ReactElement } from 'react';
+            import { ReactElement } from 'react';
+            import Link from 'next/link'
 
-    export function Footer({className}: {className:string}): ReactElement {
+                export function Footer({className}: {className:string}): ReactElement {
 
-        return (
-            <footer className={className}>
-            <div className={"border-solid border-4 border-red-500 size-full grid grid-cols-3 gap-4 "}>
-            <div>Div 1
-                <div>Nested div</div>
-            </div>
-            <div>Div 2</div>
-            <div>Div 3</div>
-            </div>
-            </footer>
-        )
+                    interface FooterElements { //Les éléments sont optionels=>pour plus de flexibilité
+                        elementName?: string
+                        url?: string
+                        image?: string 
+                    }
 
-    }
+                    const footerElements:FooterElements[] = [
+                        {
+                            url: "#",
+                            image: "/HaorixLogo.png"
+                        },
+                        {
+                            elementName: "Div 1",
+                            url: "#"
+                        },
+                        {
+                            elementName: "Div 1",
+                            url: "#"
+                        },
+                        {
+                            elementName:"Div 3",
+                            url: "#"
+                        },
+
+                        
+                    ];
+
+                    return (
+                        <footer className={className}>
+                        <div className="border-solid border-4 border-red-500 size-full grid grid-cols-3 gap-4">
+                            {footerElements.map((element, index) => (
+                                <Link 
+                                    key={index}
+                                    href={"#"}
+                                    className="text-muted-foreground hover:text-primary block duration-150"
+                                >
+                                    {element.elementName} 
+                                    {element.image && <img src={element.image} alt={element.elementName} className="w-[100px] h-auto" />
+                                    } 
+                                </Link>  
+                            ))}
+                        </div>
+                    </footer>
+                    )
+
+                }
+
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,100 +1,138 @@
-          import type { Metadata } from "next";
-          import { Geist, Geist_Mono } from "next/font/google";
-          import "./globals.css";
-          import { ThemeProvider } from "./theme-provider";
-          import { ReactElement } from 'react';
-          import {
-            NavigationMenu,
-            NavigationMenuContent,
-            NavigationMenuIndicator,
-            NavigationMenuItem,
-            NavigationMenuLink,
-            NavigationMenuList,
-            NavigationMenuTrigger,
-            NavigationMenuViewport,
-          } from "@/components/ui/navigation-menu";
-          
-          import { Footer } from "./Regular-Components/footer";
+                  import type { Metadata } from "next";
+                  import { Geist, Geist_Mono } from "next/font/google";
+                  import "./globals.css";
+                  import { ThemeProvider } from "./theme-provider";
+                  import { ReactElement } from 'react';
+                  import {
+                    NavigationMenu,
+                    NavigationMenuContent,
+                    NavigationMenuIndicator,
+                    NavigationMenuItem,
+                    NavigationMenuLink,
+                    NavigationMenuList,
+                    NavigationMenuTrigger,
+                    NavigationMenuViewport,
+                  } from "@/components/ui/navigation-menu";
+                  import { Footer } from "./Regular-Components/footer";
+                  import Image from 'next/image'
 
 
-          const geistSans = Geist({
-            variable: "--font-geist-sans",
-            subsets: ["latin"],
-          });
+                  const geistSans = Geist({
+                    variable: "--font-geist-sans",
+                    subsets: ["latin"],
+                  });
 
-          const geistMono = Geist_Mono({
-            variable: "--font-geist-mono",
-            subsets: ["latin"],
-          });
+                  const geistMono = Geist_Mono({
+                    variable: "--font-geist-mono",
+                    subsets: ["latin"],
+                  });
 
-          export const metadata: Metadata = {
-            title: "Haorix | Accueil",
-            description: "Placer la description ici pour le SEO...",//TODO: Ajouter une description
-            applicationName: "Haorix",
-            authors: [
-              {
-                name: "HAORIX.Inc",
-                url: "https://haorix123.com", //TODO: Ajouter une URL valide
-              }
-            ],
-            openGraph: {
-              type: "website",
-              url: "https://haorix123.com", //TODO: Ajouter une URL valide
-              title: "Haorix | Accueil",
-              description: "Placer la description ici pour le SEO...", //TODO: Ajouter une description
-              siteName: "Haorix",
-              images: [{ url: "https://haorix123.com/og.png" }] //TODO: Ajouter une image 
-            }
-          };
+                  export const metadata: Metadata = {
+                    title: "Haorix | Accueil",
+                    description: "Placer la description ici pour le SEO...",//TODO: Ajouter une description
+                    applicationName: "Haorix",
+                    viewport: {
+                      width: "device-width",
+                      initialScale: 1.0
+                    },
+                    authors: [
+                      {
+                        name: "HAORIX.Inc",
+                        url: "https://haorix123.com", //TODO: Ajouter une URL valide
+                      }
+                    ],
+                    openGraph: {
+                      type: "website",
+                      url: "https://haorix123.com", //TODO: Ajouter une URL valide
+                      title: "Haorix | Accueil",
+                      description: "Placer la description ici pour le SEO...", //TODO: Ajouter une description
+                      siteName: "Haorix",
+                      images: [{ url: "https://haorix123.com/og.png" }] //TODO: Ajouter une image 
+                    }
+                  };
 
-          export function NavigationBar(): ReactElement{
-            return(
-                <header>
-              <NavigationMenu viewport={false}>
-            <NavigationMenuList>
-              <NavigationMenuItem>
-                <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  <NavigationMenuLink>Link</NavigationMenuLink>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
+                  //TODO: Faire les types pour les éléments du nav
+                  //TODO: Réparer le footer (elements sans triggers)-> élément TEST
+                  //TODO: Faire responsiveness du footer
+                  //TODO: Faire responsiveness du nav
+                  //TODO: Faire responsiveness du mobile (page #1)s
+                  //TODO: Ajouter bouton dark/light mode nav
+                  //TODO: Ajouter state dans cette page pour le hamburger menu
 
-              <NavigationMenuItem>
-                <NavigationMenuTrigger>Item Two</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  <NavigationMenuLink>Link Two</NavigationMenuLink>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
-          </header>
-            )
-          }
+                  export function NavigationBar(): ReactElement{
+                    return(
+                    <header>
+                    <NavigationMenu viewport={false}>
+                    <NavigationMenuList className="space-x-1">
+                      <Image
+                      src="/HaorixLogo.png"
+                      width={150}
+                      height={100}
+                      alt="Picture of the author"
+                      className="border-solid mx-2.5 p-2"
+                      />
+                      <NavigationMenuItem>
 
-          export default function RootLayout({
-            children,
-          }: Readonly<{
-            children: React.ReactNode;
-          }>) {
-            return (
-              <html lang="fr" suppressHydrationWarning>
-                <body
-                  className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
-                >
-                <ThemeProvider
-                //Component pour le light/dark theme
-                      attribute="class"
-                      defaultTheme="system"
-                      enableSystem
-                      disableTransitionOnChange
-                    >
-                <NavigationBar /> 
-                <main className="flex-grow border-solid border-5 border-yellow-500">
-                  {children}
-                  </main>
-                  <Footer className="border-solid border-4 border-indigo-500" /> 
-                  </ThemeProvider>
-                </body>
-              </html>
-            );
-          }
+                        <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                        <NavigationMenuLink>Link One</NavigationMenuLink>
+                        </NavigationMenuContent>
+                      </NavigationMenuItem>
+
+                      <NavigationMenuItem>
+                        <NavigationMenuTrigger>Item Two</NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                          <NavigationMenuLink>Link Two</NavigationMenuLink>
+                        </NavigationMenuContent>
+                      </NavigationMenuItem>
+
+                      <NavigationMenuItem>
+                        <NavigationMenuTrigger>Item Three</NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                          <NavigationMenuLink>Link three</NavigationMenuLink>
+                        </NavigationMenuContent>
+                      </NavigationMenuItem>
+
+                      <NavigationMenuItem>
+                        <NavigationMenuTrigger>Item Four</NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                          <NavigationMenuLink>Link Four</NavigationMenuLink>
+                        </NavigationMenuContent>
+                      </NavigationMenuItem>
+
+                        <NavigationMenuLink className="bg-black"> 
+                          TEST
+                        </NavigationMenuLink>
+
+                    </NavigationMenuList>
+                  </NavigationMenu>
+                  </header>
+                    )
+                  }
+
+                  export default function RootLayout({
+                    children,
+                  }: Readonly<{
+                    children: React.ReactNode;
+                  }>) {
+                    return (
+                      <html lang="fr" suppressHydrationWarning>
+                        <body
+                          className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col bg-white overflow-x-hidden overflow-y-auto`}
+                        >
+                        <ThemeProvider
+                              attribute="class"
+                              defaultTheme="system"
+                              enableSystem
+                              disableTransitionOnChange
+                            >
+                        <NavigationBar /> 
+                        <main className="flex-grow border-solid border-[5px] border-yellow-500">
+                          {children}
+                          </main>
+                          <Footer className="border-solid border-4 border-indigo-500" /> 
+                          </ThemeProvider>
+                        </body>
+                      </html>
+                    );
+                  }
