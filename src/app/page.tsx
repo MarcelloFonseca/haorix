7a2e474ec391faada86f1089c8d8e217fc7a6441
Marcello Fonseca <marcello.fonseca@usherbrooke.ca>
2025-09-15 21:23:35 -0400

    Model responsiveness + Canvas responsiveness + IpadPro (part3)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,14 +11,17 @@ import { ModelLayout } from './Regular-Components/Model-Layout';
 //TODO: JE VAIS POUVOIR METTRE LE TOUT SUR UNE COLONNE POUR IPAD PRO (PORTRAIT) ET DEUX COLONNES POUR IPAD PRO (LANDSCAPE)
 //TODO: PAR LA SUITE, IL FAUT QUE JE REGLE LES INTERACTIONS POUR TOUT LES BREAKPOINTS (TABLETTE ET MOBILE)
 
+//TODO: CORRIGER LE LAYOUT POUR IPADPRO (APPORTER LES CORRECTIONS NECESSAIRES POUR QUE LE TOUT SOIT UNIFORME)-> MAIS ÇA FONCTIONNE AU MOINS!
+//TODO: AJOUTER LES BREAKPOINTS DANS LE MODEL3D POUR NE PAS PERMETTRE INTERACTIONS LORSQUE MOBILE/TABLETTE EST EN MODE LANDSCAPE
+
 function Home(): ReactElement {
   return (
     <section className="bg-[var(--backgroundV2)] dark:bg-popover flex flex-col w-full min-h-[60svh] border-2 border-white 
-    ipadpro:bg-pink-500 lg:min-h-[60svh] lg:h-auto lg:p-2 lg:flex lg:justify-center">
-      <div className="flex-1 grid grid-cols-1 gap-3 lg:grid-cols-2 border-2 border-blue-500 pt-7">
+    lg:min-h-[60svh] lg:h-auto lg:p-2 lg:flex lg:justify-center">
+      <div className="flex-1 grid grid-cols-1 gap-3 lg:grid-cols-2 border-2 border-blue-500 pt-7 ipadpro:flex ipadpro:flex-col ipadpro-land:flex ipadpro-land:flex-col">
         <div className="flex flex-col gap-y-6 border-2 border-purple-400 p-5">
           <h1 className={`${michroma.className} text-4xl`}>Test1</h1>
-          <p className={`${montserrat.className} text-base leading-relaxed text-pretty`}>
+          <p className={`${montserrat.className} text-base leading-relaxed text-pretty`}> 
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
             dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
             ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
