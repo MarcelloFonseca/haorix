fbad8ae3810b9f80cce6e87b153aa7eb923ac25c
Marcello Fonseca <marcello.fonseca@usherbrooke.ca>
2025-11-11 22:46:20 -0500

    Changements mineurs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,16 +10,15 @@ import { ModelLayout } from './Regular-Components/Model-Layout';
 //TODO: JE VAIS POUVOIR METTRE LE TOUT SUR UNE COLONNE POUR IPAD PRO (PORTRAIT) ET DEUX COLONNES POUR IPAD PRO (LANDSCAPE)
 //TODO: PAR LA SUITE, IL FAUT QUE JE REGLE LES INTERACTIONS POUR TOUT LES BREAKPOINTS (TABLETTE ET MOBILE)
 //TODO: CORRIGER LE LAYOUT POUR IPADPRO (APPORTER LES CORRECTIONS NECESSAIRES POUR QUE LE TOUT SOIT UNIFORME)-> MAIS ÇA FONCTIONNE AU MOINS!
-
-//TODO: Completer le floating 3d model div que j'ai commencé dans la page
-//TODO: Finalement, je vais peut-être placer le floating 3d en landscape et non en portrait sur mobile et tablette
+//TODO: Completer le floating 3d model div que j'ai commencé dans la pages
+//TODO: Finalement, je vais peut-être placer le floating 3d en landscape et non en portrait sur mobile et tablette. CONFIRMÉ !
 
 function Home(): ReactElement {
   return (
-    <section className="bg-[var(--backgroundV2)] dark:bg-popover flex flex-col w-full min-h-[60svh] border-2 border-white 
-    lg:min-h-[60svh] lg:h-auto lg:p-2 lg:flex lg:justify-center ipadpro:relative ipadpro:pb-[24svh]">
-      <div className="flex-1 grid grid-cols-1 gap-3 lg:grid-cols-2 border-2 border-blue-500 ipadpro:gap-1 pt-7 
-      ipadpro:grid ipadpro:grid-cols-1 ipadpro-land:grid ipadpro-land:grid-cols-1">
+    <section className="relative pb-[24svh] bg-[var(--backgroundV2)] dark:bg-popover flex flex-col w-full min-h-[60svh] border-2 border-white 
+    lg:min-h-[60svh] lg:h-auto lg:p-2 lg:flex lg:justify-center lg:pb-0 lg:relative">
+      <div className="flex-1 grid grid-cols-1 gap-1 pt-7 border-2 border-blue-500 
+      lg:grid-cols-2 lg:gap-3">
         <div className="flex flex-col gap-y-6 border-2 border-purple-400 p-5">
           <h1 className={`${michroma.className} text-4xl`}>Test1</h1>
           <p className={`${montserrat.className} text-base leading-relaxed text-pretty`}> 
@@ -36,8 +35,11 @@ function Home(): ReactElement {
             </button>
           </div>
         </div>
-        <div className="border-2 border-orange-400 m-0 flex flex-col items-center w-auto h-full lg:w-full lg:h-auto
-        ipadpro:absolute ipadpro:translate-y-[200px] ipadpro:translate-x-[450px] ipadpro:h-[20svh] ipadpro:w-[50svw] ipadpro:z-40">
+        <div className="absolute translate-y-[150px] translate-x-[30px] h-[30svh] w-[85svw] z-40 border-2 border-orange-400 m-0 flex flex-col items-center
+        md:translate-y-[160px] md:translate-x-[180px] md:h-[32svh] md:w-[60svw]
+        lg:relative lg:translate-y-0 lg:translate-x-0 lg:w-full lg:h-auto
+        ipadpro:translate-y-[180px] ipadpro:translate-x-[420px] ipadpro:h-[35svh] ipadpro:w-[50svw]
+        ipadpro-land:translate-y-[180px] ipadpro-land:translate-x-[650px] ipadpro-land:h-[35svh] ipadpro-land:w-[50svw]">
           <div className="relative aspect-[16/10] w-full overflow-hidden">
             <ModelLayout>
               <HDash />
@@ -45,8 +47,51 @@ function Home(): ReactElement {
           </div>
         </div>
       </div>
+      <div className='flex flex-col items-start gap-3'>
+      <div className='border-red-800'>
+        <p> test test test </p>
+      </div>
+      <div className='border-red-800'>
+        <p> test test test </p>
+      </div>
+      <div className='border-red-800'>
+        <p> test test test </p>
+      </div>
+      <div className='border-red-800'>
+        <p> test test test </p>
+      </div>
+      <div className='border-red-800'>
+        <p> test test test </p>
+      </div>
+      <div className='border-red-800'>
+        <p> test test test </p>
+      </div>
+      <div className='border-red-800'>
+        <p> test test test </p>
+      </div>
+        <div className='border-red-800'>
+        <p> test test test </p>
+      </div>
+      <div className='border-red-800'>
+        <p> test test test </p>
+      </div>
+      <div className='border-red-800'>
+        <p> test test test </p>
+      </div>
+      <div className='border-red-800'>
+        <p> test test test </p>
+      </div>
+      <div className='border-red-800'> 
+        <p> test test test </p>
+      </div>
+      <div className='border-red-800'>
+        <p> test test test </p>
+      </div>
+      </div>
+
     </section>
   );
 } 
 
 export default Home;
+
